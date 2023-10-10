eac8d98dceb262d5d2a7910a7ceed8b7c9325192
Billkolos <billooko2@gmail.com>
2023-10-10 20:19:48 +0300

    revised code for perf counter

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
-'''Task 2's module.
-'''
+"""
+Module contains an asynchronous coroutine
+"""
+
+
 import asyncio
 import time
-
-
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-        '''Computes the average runtime of wait_n.
-            '''
-                start_time = time.time()
-                    asyncio.run(wait_n(n, max_delay))
-                        return (time.time() - start_time) / n
+    """
+    measures the total execution time for wait_n(n, max_delay)
+    Args:
+        int n
+        int max_delay as arguments
+    Return: total_time / n
+    """
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter()
+    total_time = end - start
+    return total_time / n
